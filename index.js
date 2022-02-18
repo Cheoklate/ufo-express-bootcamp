75c00b49ee59e79e7356a209a6e04a726bc12a46
cheoklate <gcheok88@gmail.com>
2022-02-18 19:01:47 +0800

    Catchup revision

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const handleIncomingRequestPage = (request, response) => {
 		response.render('sighting', content);
 	});
 };
-
+	
 const handleIncomingForm = (request, response) => {
 	add('data.json', 'sightings', request.body, (err) => {
 		if (err) {
@@ -57,79 +57,79 @@ const handleIncomingForm = (request, response) => {
 // 		response.render('edit');
 // 	});
 // };
-const handleIncomingEdit = (request, response) => {
+// const handleIncomingEdit = (request, response) => {
 
-  read('data.json', (data, err) => {
-    if (err) {
-      console.log('read error', err);
-		}
-    const { index } = request.params;
-    data.sightings[index] = request.body;
-
-		write('data.json', data, (newData) => {
-			response.redirect('/');
-		});
-	});
-};
-
-// const renderIncomingEdit = (request, response) => {
-// 	// const { index } = request.params;
-// 	add('data.json', 'sightings', request.body, (err) => {
-// 		if (err) {
-// 			response.status(500).send('DB write error.');
-// 			return;
+//   read('data.json', (data, err) => {
+//     if (err) {
+//       console.log('read error', err);
 // 		}
-// 		response.render('edit');
+//     const { index } = request.params;
+//     data.sightings[index] = request.body;
+
+// 		write('data.json', data, (newData) => {
+// 			response.redirect('/');
+// 		});
 // 	});
 // };
 
-app.get('/sighting/:index/edit', (request, response) => {
-	read('data.json', (err, jsonData) => {
-	  const { index } = request.params;
-  
-	  if (jsonData.sightings == null || index >= jsonData.sightings.length || err) {
-		response.status(404).render('error');
-	  }
-  
-	  const sighting = jsonData.sightings[index];
-	  // Pass the recipe index to the edit form for the PUT request URL.
-	  sighting.index = index;
-	  const ejsData = { sighting };
-	  response.render('edit', ejsData);
+const renderIncomingEdit = (request, response) => {
+	const { index } = request.params;
+	add('data.json', 'sightings', request.body, (err) => {
+		if (err) {
+			response.status(500).send('DB write error.');
+			return;
+		}
+		response.render('edit');
 	});
-  });
+};
 
-app.delete('/sighting/:index/delete', (req, res) => {
-	read('data.json', (data) => {
-		const { index } = req.params;
-		const sightingsArr = data.sightings;
-		sightingsArr.splice(index, 1);
+// app.get('/sighting/:index/edit', (request, response) => {
+// 	read('data.json', (err, jsonData) => {
+// 	  const { index } = request.params;
+  
+// 	  if (jsonData.sightings == null || index >= jsonData.sightings.length || err) {
+// 		response.status(404).render('error');
+// 	  }
+  
+// 	  const sighting = jsonData.sightings[index];
+// 	  // Pass the recipe index to the edit form for the PUT request URL.
+// 	  sighting.index = index;
+// 	  const ejsData = { sighting };
+// 	  response.render('edit', ejsData);
+// 	});
+//   });
 
-		write('data.json', data, (newData) => {
-			res.redirect('/');
-		});
-	});
-});
+// app.delete('/sighting/:index/delete', (req, res) => {
+// 	read('data.json', (data) => {
+// 		const { index } = req.params;
+// 		const sightingsArr = data.sightings;
+// 		sightingsArr.splice(index, 1);
 
-app.get('/shapes', (_, response) => {
-  read('data.json', (err, data) => {
-    const uniqueshapes = new Set();
+// 		write('data.json', data, (newData) => {
+// 			res.redirect('/');
+// 		});
+// 	});
+// });
 
-    data.sightings.forEach((sighting) => {
-      if (sighting.shape !== undefined) {  
-        uniqueshapes.add(sighting.shape);
-      }
-    });
+// app.get('/shapes', (_, response) => {
+//   read('data.json', (err, data) => {
+//     const uniqueshapes = new Set();
 
-    response.render('shapes', { shapes: [...uniqueshapes] });
-  });
-});
+//     data.sightings.forEach((sighting) => {
+//       if (sighting.shape !== undefined) {  
+//         uniqueshapes.add(sighting.shape);
+//       }
+//     });
+
+//     response.render('shapes', { shapes: [...uniqueshapes] });
+//   });
+// });
 
 app.get('/', handleIncomingRequestIndex);
 app.get('/sighting', handleIncomingForm);
 app.post('/sighting', handleIncomingForm);
 app.get('/sightings/:index', handleIncomingRequestPage);
-// app.get('/sighting/:index/edit', renderIncomingEdit);
-app.post('/sighting/:index/edit', handleIncomingEdit);
+app.get('/sighting/:index/edit', renderIncomingEdit);
+// app.post('/sighting/:index/edit', handleIncomingEdit);
 
 app.listen(3004);
